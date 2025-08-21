74c9242a6bded1b58a7aae0c11fb8a1c7926b7bc
Vzoel Fox's <onenexion@gmail.com>
2025-08-21 10:14:07 +0700

    Create generate_session.py

---
+++ generate_session.py
@@ -0,0 +1,18 @@
+# Ambil dari my.telegram.org
+API_ID=
+API_HASH=
+
+# Ambil dari @BotFather di Telegram
+BOT_TOKEN=
+
+# URL koneksi dari MongoDB Atlas atau server Anda
+MONGO_DB_URI=
+
+# ID Telegram Anda
+OWNER_ID=
+
+# ID grup log (opsional)
+LOG_GROUP_ID=
+
+# String Sesi yang dihasilkan dari generate_session.py
+SESSION_STRING=
